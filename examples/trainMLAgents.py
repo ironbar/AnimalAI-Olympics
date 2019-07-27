b8ad22d1eac2d03e84674992f601d8fb360276bd
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2019-07-27 10:22:38 +0200

    Modified train script to use subprocess_environment

--- examples/trainMLAgents.py
+++ examples/trainMLAgents.py
@@ -5,15 +5,22 @@ from animalai.envs.arena_config import ArenaConfig
 import random
 import yaml
 import sys
+from functools import partial
+import numpy as np
+
+from animalai.envs.subprocess_environment import SubprocessUnityEnvironment
 
 
 # ML-agents parameters for training
+
 env_path = '../env/AnimalAI'
 worker_id = random.randint(1, 100)
 seed = 10
 base_port = 5005
 sub_id = 1
-run_id = 'train_example'
+n_envs = 8
+N_ARENAS = 4
+run_id = '008_multi_workers_%ienv_%iarenas' % (n_envs, N_ARENAS)
 save_freq = 5000
 curriculum_file = None
 load_model = False
@@ -23,7 +30,7 @@ lesson = 0
 run_seed = 1
 docker_target_name = None
 no_graphics = False
-trainer_config_path = 'configs/trainer_config.yaml'
+trainer_config_path = '/media/guillermo/Data/Kaggle/animalai/agents/006_ml_agents_first_steps/trainings/004_train_simple_better_conf/trainer_config.yaml'
 model_path = './models/{run_id}'.format(run_id=run_id)
 summaries_dir = './summaries'
 maybe_meta_curriculum = None
@@ -44,7 +51,7 @@ def load_config(trainer_config_path):
                                         .format(trainer_config_path))
 
 
-def init_environment(env_path, docker_target_name, no_graphics, worker_id, seed):
+def init_environment(worker_id, env_path, docker_target_name, no_graphics):
     if env_path is not None:
         # Strip out executable extensions if passed
         env_path = (env_path.strip()
@@ -55,10 +62,10 @@ def init_environment(env_path, docker_target_name, no_graphics, worker_id, seed)
     docker_training = docker_target_name is not None
 
     return UnityEnvironment(
-        n_arenas=4,             # Change this to train on more arenas
+        n_arenas=N_ARENAS,             # Change this to train on more arenas
         file_name=env_path,
         worker_id=worker_id,
-        seed=seed,
+        seed=worker_id,
         docker_training=docker_training,
         play=False
     )
@@ -71,7 +78,9 @@ else:
     arena_config_in = ArenaConfig('configs/exampleTraining.yaml')
 
 trainer_config = load_config(trainer_config_path)
-env = init_environment(env_path, docker_target_name, no_graphics, worker_id, run_seed)
+env_factory = partial(init_environment, docker_target_name=docker_target_name, no_graphics=no_graphics, env_path=env_path)
+env = SubprocessUnityEnvironment(env_factory, n_envs)
+# env = init_environment(worker_id, env_path, docker_target_name, no_graphics)
 
 external_brains = {}
 for brain_name in env.external_brain_names:
@@ -83,3 +92,4 @@ tc = TrainerController(model_path, summaries_dir, run_id + '-' + str(sub_id),
                        load_model, train_model,
                        keep_checkpoints, lesson, external_brains, run_seed, arena_config_in)
 tc.start_learning(env, trainer_config)
+env.close()
